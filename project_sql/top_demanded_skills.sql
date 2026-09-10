7028cfd14945e45b6b1b4c75c963605c679a7c32
Zarchi LinHtet <zar06306@gmail.com>
2026-09-10 21:07:09 +0800

    commit

--- project_sql/top_demanded_skills.sql
+++ project_sql/top_demanded_skills.sql
@@ -1,50 +1,38 @@
 /* 
-
-What are the most in-demand skills for data analysts?
-- Join job postings to inner join table similar to query 2
-- Identify the top 5 in-demand skills for a data analyst
-- Focus on all job postings.
-- Why? Retrieves the top 5 skills with the highest demand in the job market.
-- providing insights into the most valuable skills for job seekers.
-
-*/
-
-SELECT 
-    skills,
+ 
+ What are the most in-demand skills for data analysts?
+ - Join job postings to inner join table similar to query 2
+ - Identify the top 5 in-demand skills for a data analyst
+ - Focus on all job postings.
+ - Why? Retrieves the top 5 skills with the highest demand in the job market.
+ - providing insights into the most valuable skills for job seekers.
+ 
+ */
+SELECT skills,
     COUNT(skills_job_dim.job_id) AS demand_count
 FROM job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE 
-    job_title_short = 'Data Analyst' AND
-    job_work_from_home = True
-GROUP BY 
-    skills
-ORDER BY 
-    demand_count DESC
+WHERE job_title_short = 'Data Analyst'
+    AND job_work_from_home = True
+GROUP BY skills
+ORDER BY demand_count DESC
 LIMIT 5;
-
 /*
-ANSWER: What are the top skills based on salary?
-- Look at the average salary associated with each sills for data analyst positions
-- Focuses on roles with specified salaries, regardless of location
-- Why? It reveals how different skills impact salary levels for Data Analyst and
-helps identify the most financially rewarding skills to acquire or improve
-*/
-
-SELECT 
-    skills,
+ ANSWER: What are the top skills based on salary?
+ - Look at the average salary associated with each sills for data analyst positions
+ - Focuses on roles with specified salaries, regardless of location
+ - Why? It reveals how different skills impact salary levels for Data Analyst and
+ helps identify the most financially rewarding skills to acquire or improve
+ */
+SELECT skills,
     ROUND(AVG(salary_year_avg), 0) AS avg_salary
-    
 FROM job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE 
-    job_title_short = 'Data Analyst' 
+WHERE job_title_short = 'Data Analyst'
     AND salary_year_avg IS NOT NULL
     AND job_work_from_home = True
-GROUP BY 
-    skills
-ORDER BY 
-    avg_salary DESC
+GROUP BY skills
+ORDER BY avg_salary DESC
 LIMIT 25;
